bedf7ec1abff2b2f98539e4d83a0db0757a71f3f
NAVEEN KUMAR <nsheorann98@gmail.com>
2025-09-19 12:07:55 +0530

    Add files via upload

---
+++ lab2/q1a.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int length, breadth, area, perimeter;
+    printf("Enter length of rectangle: ");
+    scanf("%d", &length);
+
+    printf("Enter breadth of rectangle: ");
+    scanf("%d", &breadth);
+
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+
+    
+    printf("Area = %d\n", area);
+    printf("Perimeter = %d\n", perimeter);
+
+    return 0;
+}
